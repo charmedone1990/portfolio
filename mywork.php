ff7b9f58b439b67622a637707e2e361968098f7e
Jackie Sproule <charmedone1990@gmail.com>
2014-07-30 22:08:45 +0100

    Fixed links

--- mywork.php
+++ mywork.php
@@ -16,10 +16,10 @@
                 <h1>My Work</h1>
                 <p>To view some of the projects that I have completed, just click on the banners below. These will go into more detail about each of the projects that I have finished.</p>
 
-                <a href ="http://jackie.britintel.co.uk/halesowen.html"><img src="images/halesowenbanner.jpg" height="100" width="240" title="Halesowen" /></a>
-                <a href ="http://jackie.britintel.co.uk/lpphotography.html"><img src="images/lpphotographylogo.jpg" height="100" width="240" title="LP Photography" /></a>
-                <a href ="http://jackie.britintel.co.uk/zend.html"><img src="images/zendprojectlogo.jpg" height="100" width="240" title="Zend Project" /></a>
-                <a href ="http://jackie.britintel.co.uk/attiss.html"><img src="images/attis.jpg" height="100" width="240" title="A.T.T.I.S.S" /></a>
+                <a href ="halesowen.html"><img src="images/halesowenbanner.jpg" height="100" width="240" title="Halesowen" /></a>
+                <a href ="lpphotography.html"><img src="images/lpphotographylogo.jpg" height="100" width="240" title="LP Photography" /></a>
+                <a href ="zend.html"><img src="images/zendprojectlogo.jpg" height="100" width="240" title="Zend Project" /></a>
+                <a href ="attiss.html"><img src="images/attis.jpg" height="100" width="240" title="A.T.T.I.S.S" /></a>
                 <a href ="http://www.creativedisney.blogspot.co.uk/"><img src="images/creativedisney.jpg" height="100" width="240" title="Creative Disney" /></a>
             </div>
             <? include 'footer.php' ?>
